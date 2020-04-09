a4a5bb76ed78c436edbe0d6560c3866d88057b08
Casey D Boyer <caseyb1101@gmail.com>
2020-04-09 11:18:48 -0500

    Implemented RESP3 types: Null, Double, Boolean, Blob Error, Verbatim String, Map, and Set

--- src/core/protocol.ts
+++ src/core/protocol.ts
@@ -42,10 +42,6 @@ export class Protocol {
 }
 
 export class RedisProtocolError extends Error {
-  public static fromMessage(message: string) {
-    const space = message.indexOf(" ");
-    return new RedisProtocolError(message.slice(0, space), message.slice(space + 1));
-  }
   constructor(name: string, message: string) {
     super();
     this.name = name;
@@ -57,15 +53,26 @@ class ProtocolParser {
   public static parse(raw: string): InterfaceParser {
     let masterRegex = [
       "\\+(?<simple>.+?)",
-      "\\-(?<error>.+?)",
-      "\\:(?<int>-?\\d+)",
       "(?<blobString>\\$blobRef_\\d+)", // Note: this matches our replaced Blob-Ref
+      "(?<verbatimString>\\=blobRef_\\d+)", // Note: this matches our replaced Blob-Ref
+      "\\-(?<error_code>.+?) (?<error_msg>.+?)",
+      "(?<blobError>\\!blobRef_\\d+)", // Note: this matches our replaced Blob-Ref
+      "\\:(?<int>-?\\d+)",
+      "\\((?<bigInt>-?\\d+)",
+      ",(?<double>\\d+\.?\\d*)",
+      ",(?<inf>-?inf)",
+      "#(?<boolean>t|f)",
       "\\*(?<array_n>\\d+)(?<array>)",
+      "%(?<map_n>\\d+)(?<map>)",
+      "~(?<set_n>\\d+)(?<set>)",
+      "_(?<null>)",
       "(\\$|\\*)(?<null_string>-1)",
     ].join("|");
 
     // Preprocess blobs since they can include <CR><LF>
-    const blobs = ProtocolParser.extractBlobs(raw, "$");
+    let blobs = ProtocolParser.extractBlobs(raw, "$");
+    blobs = ProtocolParser.extractBlobs(blobs.buffer, "!", blobs.output);
+    blobs = ProtocolParser.extractBlobs(blobs.buffer, "=", blobs.output);
 
     masterRegex = `${respStart}(?:${masterRegex})${respEnd}`;
     const matches = Array.from(blobs.buffer.matchAll(new RegExp(masterRegex, "g")));
@@ -130,16 +137,72 @@ class ProtocolParser {
         if (arrayResp.length === array_n) {
           output.push(arrayResp);
         }
-      } else if ("blobString" in current.groups && current.groups.blobString !== undefined) {
-        output.push(parsed.blobs.get(current.groups.blobString));
-      } else if ("null_string" in current.groups && current.groups.null_string !== undefined) {
-        output.push(null);
-      } else if ("error" in current.groups && current.groups.error !== undefined) {
-        output.push(RedisProtocolError.fromMessage(current[0]));
-      } else if ("int" in current.groups && current.groups.int !== undefined) {
-        output.push(parseInt(current.groups.int, 10));
+      } else if ("map_n" in current.groups && current.groups.map_n !== undefined) {
+        const map_n = parseInt(current.groups.map_n, 10);
+        const mapResp = new Map();
+        for (let inx = 0; inx < map_n; inx++) {
+          const tmp = new Array();
+          ProtocolParser.aggregateMessages(parsed, tmp);
+          const field = tmp.shift();
+          ProtocolParser.aggregateMessages(parsed, tmp);
+          const value = tmp.shift();
+          mapResp.set(field, value);
+        }
+
+        if (mapResp.size === map_n) {
+          output.push(mapResp);
+        }
+      } else if ("set_n" in current.groups && current.groups.set_n !== undefined) {
+        const set_n = parseInt(current.groups.set_n, 10);
+        const setResp = new Set();
+        let collected = 0;
+        for (let inx = 0; inx < set_n; inx++) {
+          const tmp = new Array();
+          ProtocolParser.aggregateMessages(parsed, tmp);
+          setResp.add(tmp.shift());
+          collected++;
+        }
+
+        if (collected === set_n) {
+          output.push(setResp);
+        }
       } else if ("simple" in current.groups && current.groups.simple !== undefined) {
         output.push(current.groups.simple);
+      } else if ("blobString" in current.groups && current.groups.blobString !== undefined) {
+        output.push(parsed.blobs.get(current.groups.blobString));
+      } else if ("verbatimString" in current.groups && current.groups.verbatimString !== undefined) {
+        // TODO: We could have some sort of escaped Object which can handle txt vs mkd
+        output.push(parsed.blobs.get(current.groups.verbatimString)?.slice(4, ));
+      } else if ("error_code" in current.groups && current.groups.error_code !== undefined) {
+        output.push(new RedisProtocolError(current.groups.error_code, current.groups.error_msg));
+      } else if ("blobError" in current.groups && current.groups.blobError !== undefined) {
+        const errorBlob = parsed.blobs.get(current.groups.blobError) || "";
+        const errorParts = errorBlob.match(/^(?<code>.+?) (?<msg>.+?)$/s);
+        if (errorParts?.groups !== undefined) {
+          output.push(new RedisProtocolError(errorParts.groups.code, errorParts.groups.msg));
+        }
+      } else if ("int" in current.groups && current.groups.int !== undefined) {
+        output.push(parseInt(current.groups.int, 10));
+      } else if ("bigInt" in current.groups && current.groups.bigInt !== undefined) {
+        output.push(BigInt(current.groups.bigInt));
+      } else if ("double" in current.groups && current.groups.double !== undefined) {
+        output.push(parseFloat(current.groups.double));
+      } else if ("inf" in current.groups && current.groups.inf !== undefined) {
+        if (current.groups.inf[0] === "-") {
+          output.push(-Infinity);
+        } else {
+          output.push(Infinity);
+        }
+      } else if ("boolean" in current.groups && current.groups.boolean !== undefined) {
+        if (current.groups.boolean === "t") {
+          output.push(true);
+        } else if (current.groups.boolean === "f") {
+          output.push(false);
+        }
+      } else if ("null" in current.groups && current.groups.null !== undefined) {
+        output.push(null);
+      } else if ("null_string" in current.groups && current.groups.null_string !== undefined) {
+        output.push(null);
       }
     }
   }
--- test/core/protocol.spec.ts
+++ test/core/protocol.spec.ts
@@ -16,7 +16,59 @@ beforeEach(async () => {
 
 describe("parse", () => {
   // Tests based on specification https://redis.io/topics/protocol
-  describe("RESP Simple Strings", () => {
+  // Adding RESP3 Support: https://github.com/antirez/RESP3/blob/master/spec.md
+
+  // #region Simple Types
+  describe("RESP3 Blob (Bulk) Strings", () => {
+    /**
+     * Bulk Strings are encoded in the following way:
+     *  - A "$" byte followed by the number of bytes composing the string
+     *    (a prefixed length), terminated by CRLF.
+     *  - The actual string data.
+     *  - A final CRLF.
+     *
+     * RESP Bulk Strings can also be used in order to signal non-existence of a
+     * value using a special format that is used to represent a Null value. In
+     * this special format the length is -1, and there is no data ... This is
+     * called a **Null Bulk String**. The client library API should not return
+     * an empty string, but a nil object, when the server replies with a Null
+     * Bulk String. For example a Ruby library should return 'nil' while a C
+     * library should return NULL (or set a special flag in the reply object),
+     * and so forth.
+     */
+    it(`$6 foobar`, () => {
+      protocol.write(Buffer.from(`$6\r\nfoobar\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual(["foobar"]);
+    });
+    it(`$0`, () => {
+      protocol.write(Buffer.from(`$0\r\n\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([""]);
+    });
+    it(`$-1`, () => {
+      protocol.write(Buffer.from(`$-1\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([null]);
+    });
+    it(`$ includes CRLF`, () => {
+      protocol.write(Buffer.from(`$13\r\nhello\r\nworld!\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual(["hello\r\nworld!"]);
+    });
+    it(`$ includes LF and tab`, () => {
+      protocol.write(Buffer.from(`$14\r\nhello\n\t\tworld!\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual(["hello\n\t\tworld!"]);
+    });
+    it(`$ incomplete`, () => {
+      protocol.write(Buffer.from(`$3\r\nhe`));
+      data = protocol.parse();
+      expect(data).toEqual([]);
+    });
+  });
+
+  describe("RESP3 Simple Strings", () => {
     /**
      * When Redis replies with a Simple String, a client library should return
      * to the caller a string composed of the first character after the '+' up
@@ -34,7 +86,7 @@ describe("parse", () => {
     });
   });
 
-  describe("RESP Errors", () => {
+  describe("RESP3 Simple Errors", () => {
     /**
      * ... errors are treated by clients as exceptions, and the string that
      * composes the Error type is the error message itself.
@@ -59,7 +111,7 @@ describe("parse", () => {
     });
   });
 
-  describe("RESP Integers", () => {
+  describe("RESP3 Numer (Integers)", () => {
     /**
      * This type is just a CRLF terminated string representing an integer,
      * prefixed by a ":" byte. For example ":0\r\n", or ":1000\r\n" are integer
@@ -95,47 +147,63 @@ describe("parse", () => {
     });
   });
 
-  describe("RESP Bulk Strings", () => {
+  describe("RESP3 Null", () => {
     /**
-     * Bulk Strings are encoded in the following way:
-     *  - A "$" byte followed by the number of bytes composing the string
-     *    (a prefixed length), terminated by CRLF.
-     *  - The actual string data.
-     *  - A final CRLF.
-     *
-     * RESP Bulk Strings can also be used in order to signal non-existence of a
-     * value using a special format that is used to represent a Null value. In
-     * this special format the length is -1, and there is no data ... This is
-     * called a **Null Bulk String**. The client library API should not return
-     * an empty string, but a nil object, when the server replies with a Null
-     * Bulk String. For example a Ruby library should return 'nil' while a C
-     * library should return NULL (or set a special flag in the reply object),
-     * and so forth.
+     * The null type is encoded just as _\r\n, which is just the underscore
+     * character followed by the CR and LF characters.
      */
-    it(`$6 foobar`, () => {
-      protocol.write(Buffer.from(`$6\r\nfoobar\r\n`));
-      data = protocol.parse();
-      expect(data).toEqual(["foobar"]);
-    });
-    it(`$0 Empty String`, () => {
-      protocol.write(Buffer.from(`$0\r\n\r\n`));
-      data = protocol.parse();
-      expect(data).toEqual([""]);
-    });
-    it(`$-1`, () => {
-      protocol.write(Buffer.from(`$-1\r\n`));
+    it(`_`, () => {
+      protocol.write(Buffer.from(`_\r\n`));
       data = protocol.parse();
       expect(data).toEqual([null]);
     });
-    it(`$ includes CRLF`, () => {
-      protocol.write(Buffer.from(`$13\r\nhello\r\nworld!\r\n`));
+  });
+
+  describe("RESP3 Double", () => {
+    /**
+     * The general form is ,<floating-point-number>\r\n.
+     *
+     * To just start with . assuming an initial zero is invalid. Exponential format is invalid.
+     */
+    it(`,1.23`, () => {
+      protocol.write(Buffer.from(`,1.23\r\n`));
       data = protocol.parse();
-      expect(data).toEqual([`hello\r\nworld!`]);
+      expect(data).toEqual([1.23]);
     });
-    it(`$ incomplete`, () => {
-      protocol.write(Buffer.from(`$3\r\nhe`));
+
+    it(`,10`, () => {
+      protocol.write(Buffer.from(`,10\r\n`));
       data = protocol.parse();
-      expect(data).toEqual([]);
+      expect(data).toEqual([10]);
+    });
+
+    it(`,inf`, () => {
+      protocol.write(Buffer.from(`,inf\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([Infinity]);
+    });
+
+    it(`,-inf`, () => {
+      protocol.write(Buffer.from(`,-inf\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([-Infinity]);
+    });
+  });
+
+  describe("RESP3 Boolean", () => {
+    /**
+     * True and false values are just represented using #t\r\n and #f\r\n sequences.
+     */
+    it(`#t`, () => {
+      protocol.write(Buffer.from(`#t\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([true]);
+    });
+
+    it(`#f`, () => {
+      protocol.write(Buffer.from(`#f\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([false]);
     });
     it(`$ incomplete`, () => {
       protocol.write(Buffer.from(`$9\r\n世界！\r\n`));
@@ -145,7 +213,83 @@ describe("parse", () => {
 
   });
 
-  describe("RESP Arrays", () => {
+  describe("RESP3 Blob Errors", () => {
+    /**
+     * The general form is !<length>\r\n<bytes>\r\n.
+     */
+    it(`!21 SYNTAX invalid syntax`, () => {
+      protocol.write(Buffer.from(`!21\r\nSYNTAX invalid syntax\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([new RedisProtocolError("SYNTAX", "invalid syntax")]);
+    });
+    it(`!7 FOO bar`, () => {
+      protocol.write(Buffer.from(`!7\r\nFOO bar\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([new RedisProtocolError("FOO", "bar")]);
+    });
+    it(`! includes CRLF`, () => {
+      protocol.write(Buffer.from(`!20\r\nHELLO \r\nSome\n\tworld!\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([new RedisProtocolError("HELLO", "\r\nSome\n\tworld!")]);
+    });
+  });
+
+  describe("RESP3 Verbatim Strings", () => {
+    /**
+     * This is exactly like the Blob string type, but the initial byte is = instead of $.
+     * Moreover the first three bytes provide information about the format of the following string,
+     * which can be txt for plain text, or mkd for markdown. The fourth byte is always :. Then the
+     * real string follows.
+     *
+     * Normal client libraries may ignore completely the difference between this type and the String
+     * type, and return a string in both cases.
+     */
+    it(`=15 txt:Some string`, () => {
+      protocol.write(Buffer.from(`=15\r\ntxt:Some string\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual(["Some string"]);
+    });
+    it(`= includes CRLF`, () => {
+      protocol.write(Buffer.from(`=19\r\nmkd:# hello\r\nworld!\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual(["# hello\r\nworld!"]);
+    });
+  });
+
+  describe("RESP3 Big Number", () => {
+    /**
+     * The general form is (<big number>\r\n
+     */
+    it(`(0`, () => {
+      protocol.write(Buffer.from(`(0\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([BigInt(0)]);
+    });
+    it(`(1000`, () => {
+      protocol.write(Buffer.from(`(1000\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([BigInt(1000)]);
+    });
+    it(`(-1`, () => {
+      protocol.write(Buffer.from(`(-1\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([BigInt(-1)]);
+    });
+    it(`(-3492890328409238509324850943850943825024385`, () => {
+      protocol.write(Buffer.from(`(-3492890328409238509324850943850943825024385\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([BigInt("-3492890328409238509324850943850943825024385")]);
+    });
+    it(`(3492890328409238509324850943850943825024385`, () => {
+      protocol.write(Buffer.from(`(3492890328409238509324850943850943825024385\r\n`));
+      data = protocol.parse();
+      expect(data).toEqual([BigInt("3492890328409238509324850943850943825024385")]);
+    });
+  });
+  // #endregion Simple Types
+
+  // #region Aggregate Types
+  describe("RESP3 Arrays", () => {
     /**
      * RESP Arrays are sent using the following format:
      *  - A '*' character as the first byte, followed by the number of elements in the array as a decimal number,
@@ -211,6 +355,83 @@ describe("parse", () => {
       expect(data).toEqual([]);
     });
   });
+
+  describe("RESP3 Map", () => {
+    /**
+     * Maps represent a sequence of field-value items, basically what we could call a
+     * dictionary data structure, or in other terms, an hash.
+     */
+    it(`%{first:1,second:2}`, () => {
+      protocol.write(Buffer.from(`%2\r\n+first\r\n:1\r\n+second\r\n:2\r\n`));
+      data = protocol.parse();
+      expect(data).toMatchObject([new Map(Object.entries({first: 1, second: 2}))]);
+    });
+    it(`% Complex arrays and blobs`, () => {
+      protocol.write(Buffer.from(`%3\r\n+first\r\n*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n+second\r\n*3\r\n:1\r\n1\r\n$5\r\nhello\r\n$5\r\ntedis\r\n+third\r\n=19\r\nmkd:# hello\r\nworld!\r\n`));
+      data = protocol.parse();
+      expect(data).toMatchObject([new Map(Object.entries({first: ["foo", "bar"], second: [1, "hello", "tedis"], third: "# hello\r\nworld!"}))]);
+    });
+  });
+
+  describe("RESP3 Sets", () => {
+    /**
+     * Sets are exactly like the Array type, but the first byte is ~ instead of *
+     *
+     * However they are semantically different because the represented items are unordered collections
+     * of elements, so the client library should return a type that, while not necessarily ordered, has
+     * a test for existence operation running in constant or logarithmic time.
+     */
+    it(`~0 [] Empty Set`, () => {
+      protocol.write(
+        Buffer.from(`~0\r\n`)
+      );
+      data = protocol.parse();
+      expect(data).toEqual([new Set()]);
+    });
+    it(`~2 [foo bar]`, () => {
+      protocol.write(
+        Buffer.from(`~2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n`)
+      );
+      data = protocol.parse();
+      expect(data).toEqual([new Set(["foo", "bar"])]);
+    });
+    it(`~3 [1 2 3]`, () => {
+      protocol.write(
+        Buffer.from(`~3\r\n:1\r\n:2\r\n:3\r\n`)
+      );
+      data = protocol.parse();
+      expect(data).toEqual([new Set([1, 2, 3])]);
+    });
+    it(`~5 [1 2 3 4 foobar]`, () => {
+      protocol.write(
+        Buffer.from(`~5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$6\r\nfoobar\r\n`)
+      );
+      data = protocol.parse();
+      expect(data).toEqual([new Set([1, 2, 3, 4, "foobar"])]);
+    });
+    it(`~3 foo null bar`, () => {
+      protocol.write(
+        Buffer.from(`~3\r\n$3\r\nfoo\r\n_\r\n$3\r\nbar\r\n`)
+      );
+      data = protocol.parse();
+      expect(data).toEqual([new Set(["foo", null, "bar"])]);
+    });
+    it(`~5 orange apple true 100 999`, () => {
+      protocol.write(
+        Buffer.from(`~5\r\n+orange\r\n+apple\r\n#t\r\n:100\r\n:999\r\n`)
+      );
+      data = protocol.parse();
+      expect(data).toEqual([new Set(["orange", "apple", true, 100, 999])]);
+    });
+    it(`~3 orange orange`, () => {
+      protocol.write(
+        Buffer.from(`~3\r\n+orange\r\n+orange\r\n+apple\r\n`)
+      );
+      data = protocol.parse();
+      expect(data).toEqual([new Set(["orange", "apple"])]);
+    });
+  });
+  // #endregion Aggregate Types
 });
 
 describe("encode", () => {
